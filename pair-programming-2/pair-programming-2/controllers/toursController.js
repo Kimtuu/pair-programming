80dcbd228c969cd9c6a849fe0dca374180f92647
Valerio Tatananni <valerio.tatananni@gmail.com>
2023-11-17 16:51:35 +0200

    Added functionality to toursController and toursRoutes

--- pair-programming-2/pair-programming-2/controllers/toursController.js
+++ pair-programming-2/pair-programming-2/controllers/toursController.js
@@ -1,71 +1,115 @@
-const uuid = require("uuid");
-const tours = require("../models/tours");
+const Tours = require("../models/toursModel");
 
 // Get All tours
-const getAlltours = (req, res) => {
-  res.json(tours);
-};
-
-// Get Single tour by ID
-const gettourById = (req, res) => {
-  const found = tours.some((tour) => tour.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(tours.filter((tour) => tour.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No tour with the id of ${req.params.id}` });
+const getTours = async (req, res) => {
+  try {
+    const tours = await Tours.find();
+    res.json(tours);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
-// Create a New tour
-const createtour = (req, res) => {
-  const newtour = {
-    id: uuid.v4(),
-    ...req.body,
-  };
-
-  if (!newtour.title || !newtour.location) {
-    return res.status(400).json({ msg: "Please include a title and location" });
-  }
-
-  tours.push(newtour);
-  res.json(tours);
-};
-
-// Update tour by ID
-const updatetour = (req, res) => {
-  const found = tours.some((tour) => tour.id === parseInt(req.params.id));
-
-  if (found) {
-    tours.forEach((tour, i) => {
-      if (tour.id === parseInt(req.params.id)) {
-        tours[i] = { ...tour, ...req.body };
-        res.json({ msg: "tour updated", tour: tours[i] });
-      }
+// create a service
+const createTour = async (req, res) => {
+  try {
+    const { image, date, title, info, location, duration, cost } = req.body;
+    if (!image || !date || !title || !info || !location || !duration || !cost) {
+      return res.status(400).json({ msg: "Please include all fields" });
+    }
+    const newTour = new Tours({
+      image,
+      date,
+      title,
+      info,
+      location,
+      duration,
+      cost,
     });
-  } else {
-    res.status(400).json({ msg: `No tour with the id of ${req.params.id}` });
+    const savedTour = await newTour.save();
+    res.status(201).json(savedTour);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
-// Delete tour by ID
-const deletetour = (req, res) => {
-  const found = tours.some((tour) => tour.id === parseInt(req.params.id));
+// update a tour (PUT)
+const updateTour = async (req, res) => {
+  try {
+    const { image, date, title, info, location, duration, cost } = req.body;
+    if (!image || !date || !title || !info || !location || !duration || !cost) {
+      return res.status(400).json({ msg: "Please include all fields" });
+    }
+    const tour = await Tours.findById(req.params.id);
+    if (!tour) {
+      return res.status(404).json({ msg: "Tour not found" });
+    }
+    tour.image = image;
+    tour.date = date;
+    tour.title = title;
+    tour.info = info;
+    tour.location = location;
+    tour.duration = duration;
+    tour.cost = cost;
+    const savedTour = await tour.save();
+    res.json(savedTour);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-  if (found) {
-    const updatedtours = tours.filter(
-      (tour) => tour.id !== parseInt(req.params.id)
-    );
-    res.json({ msg: "tour deleted", tours: updatedtours });
-  } else {
-    res.status(400).json({ msg: `No tour with the id of ${req.params.id}` });
+// patch a tour
+
+const patchTour = async (req, res) => {
+  try {
+    const tour = await Tours.findById(req.params.id);
+    if (!tour) {
+      return res.status(404).json({ msg: "Tour not found" });
+    }
+    
+    // Apply updates from the request
+    Object.assign(tour, req.body);
+
+    const savedTour = await tour.save();
+    res.json(savedTour);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// delete a tour
+const deleteTour = async (req, res) => {
+  try {
+    const tour = await Tours.findByIdAndDelete(req.params.id);
+    if (!tour) {
+      return res.status(404).json({ error: "Tour not found" });
+    }
+    res.json({ message: "Tour deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// get tour by id
+
+const getTourById = async (req, res) => {
+  try {
+    const tour = await Tours.findById(req.params.id);
+    if (!tour) {
+      return res.status(404).json({ error: "Service not found" });
+    }
+    res.json(tour);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 module.exports = {
-  getAlltours,
-  gettourById,
-  createtour,
-  updatetour,
-  deletetour,
+  getTours,
+  createTour,
+  updateTour,
+  patchTour,
+  deleteTour,
+  getTourById,
 };
--- pair-programming-2/pair-programming-2/routers/toursRoutes.js
+++ pair-programming-2/pair-programming-2/routers/toursRoutes.js
@@ -1,22 +1,27 @@
 const express = require("express");
-const tourController = require("../controllers/toursController");
-const { checkRole } = require("../middleware/rolesMiddleware");
+const toursController = require("../controllers/toursController");
+//const { checkRole } = require("../middleware/rolesMiddleware");
 
 const router = express.Router();
 
 // Get all tours
-router.get("/", tourController.getAlltours);
+router.get("/", toursController.getTours);
 
 // Get a single tour by ID
-router.get("/:id", tourController.gettourById);
+router.get("/:id", toursController.getTourById);
 
 // Create a new tour
-router.post("/", checkRole("admin"), tourController.createtour);
+router.post("/", toursController.createTour);
 
 // Update a tour by ID
-router.put("/:id", checkRole("admin"), tourController.updatetour);
+router.put("/:id", toursController.updateTour);
+
+// Patch a tour by ID
+router.patch("/:id", toursController.patchTour);
 
 // Delete a tour by ID
-router.delete("/:id", checkRole("admin"), tourController.deletetour);
+router.delete("/:id", toursController.deleteTour);
+
+//checkRole("admin")
 
 module.exports = router;
